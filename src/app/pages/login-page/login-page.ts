809ccaca1f3280540b2434c70846f6fb0be6be95
Jwoliv <bohdanlishchuk0@gmail.com>
2025-08-10 13:27:23 +0300

    add mechanism for revealing password

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Output} from '@angular/core';
+import {Component, inject, Output, signal} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../service/auth/auth-service';
 import {TokenResponse} from '../../interfaces/model/token-response';
@@ -15,30 +15,30 @@ import {LoginRequest} from '../../interfaces/model/login-request';
   styleUrl: './login-page.scss'
 })
 export class LoginPage {
-  private tokenResponse: TokenResponse = {
-    access_token: '',
-    refresh_token: '',
-    token_type: ''
-  };
   private authService = inject(AuthService)
   private router = inject(Router)
+
+  public isPasswordVisible = signal(false);
 
   public loginForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
   })
 
-  sendLoginRequest(event: Event) {
+  public sendLoginRequest(event: Event) {
     let loginRequest: LoginRequest = {
       username: this.loginForm.value.username,
       password: this.loginForm.value.password,
     };
     this.authService.getTokenByLogin(loginRequest).subscribe({
       next: (response: TokenResponse) => {
-        this.tokenResponse = response
         this.authService.saveTokenInCookie(response);
         this.router.navigate(['/search'])
       }
     });
+  }
+
+  public clickPasswordRevealButton(): void {
+    this.isPasswordVisible.set(!this.isPasswordVisible())
   }
 }
